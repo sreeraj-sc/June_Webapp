741fedf98b66953af58c9242e405b3254c1d17f0
d4rkd3vil <sreerajsc5@gmail.com>
2024-03-10 20:11:15 +0530

    conflict in admin si changed the name to root and change the configuration but the urls include is not working

--- root/urls.py
+++ root/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from user import views
-from root import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user.urls')),
-    path('',include('root.urls'))
+
 ]
